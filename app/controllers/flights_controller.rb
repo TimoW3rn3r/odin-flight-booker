e10a556762e2b9ca0ceda5178b9957c618107101
Werner <turbotimoh@gmail.com>
2023-04-18 19:29:34 +0545

    feat: find and display flights using search params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,5 +3,12 @@ class FlightsController < ApplicationController
     @airports = Airport.all.order(:name)
     @flights = Flight.select(:date).distinct.order(:date)
     @passengers_options = (1..4).map { |x| [x, x]}
+
+    @flight_results = Flight.where(
+      "departure_airport_id = ? AND arrival_airport_id = ? AND date = ?",
+        params[:departure_airport_id],
+        params[:arrival_airport_id],
+        params[:date],
+    )
   end
 end
