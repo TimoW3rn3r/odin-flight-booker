42772d1aa1fea4da09725c2129c8dcbc383ab63a
Werner <turbotimoh@gmail.com>
2023-04-18 17:57:10 +0545

    feat: create `flights#index` page for searching flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,7 @@
 class FlightsController < ApplicationController
+  def index
+    @airports = Airport.all.order(:name)
+    @flights = Flight.select(:date).distinct.order(:date)
+    @passengers_options = (1..4).map { |x| [x, x]}
+  end
 end
