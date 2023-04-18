641ff090fb9a5ca501c4ce5f82294c68fadc0e37
Werner <turbotimoh@gmail.com>
2023-04-18 14:18:47 +0545

    Fix: migration to support deletion of references

--- db/migrate/20230418063936_create_flights.rb
+++ db/migrate/20230418063936_create_flights.rb
@@ -1,8 +1,8 @@
 class CreateFlights < ActiveRecord::Migration[7.0]
   def change
     create_table :flights do |t|
-      t.references :departure_airport, null: false, foreign_key: { to_table: :airports }
-      t.references :arrival_airport, null: false, foreign_key: { to_table: :airports }
+      t.references :departure_airport, null: false, foreign_key: { to_table: :airports, on_delete: :cascade }
+      t.references :arrival_airport, null: false, foreign_key: { to_table: :airports, on_delete: :cascade }
       t.date :date
       t.time :time
       t.integer :duration
