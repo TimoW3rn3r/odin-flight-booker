cda6c5637093a728ca226dfb098bdc08740202ee
Werner <turbotimoh@gmail.com>
2023-04-18 12:42:49 +0545

    Create Flight Model and migrate it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_061830) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_063936) do
   create_table "airports", force: :cascade do |t|
     t.string "airport_code", default: "", null: false
     t.string "airport_name", default: "", null: false
@@ -18,4 +18,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_061830) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.integer "departure_airport_id", null: false
+    t.integer "arrival_airport_id", null: false
+    t.date "date"
+    t.time "time"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "arrival_airports"
+  add_foreign_key "flights", "departure_airports"
 end
