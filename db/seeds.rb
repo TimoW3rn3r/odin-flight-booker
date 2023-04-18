af918338af838ce2da8e2a9e23cc5d0032c95e81
Werner <turbotimoh@gmail.com>
2023-04-18 19:46:08 +0545

    fix: decrease airports to increase match probabiliy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,6 @@ airports = [
   { name: 'Charles de Gaulle Airport', code: 'CDG' },
   { name: 'Los Angeles International Airport', code: 'LAX' },
   { name: 'Dubai International Airport', code: 'DXB' },
-  { name: 'Beijing Capital International Airport', code: 'PEK' },
-  { name: 'Frankfurt Airport', code: 'FRA' },
-  { name: 'Narita International Airport', code: 'NRT' },
-  { name: 'Sydney Airport', code: 'SYD' },
-  { name: "O'Hare International Airport", code: 'ORD' },
-  { name: 'Hong Kong International Airport', code: 'HKG' },
-  { name: 'Amsterdam Airport Schiphol', code: 'AMS' },
-  { name: 'Singapore Changi Airport', code: 'SIN' },
-  { name: 'Incheon International Airport', code: 'ICN' },
-  { name: 'Istanbul Airport', code: 'IST' },
-  { name: 'Denver International Airport', code: 'DEN' },
-  { name: 'Suvarnabhumi Airport', code: 'BKK' },
-  { name: 'Toronto Pearson International Airport', code: 'YYZ' },
-  { name: 'Shanghai Pudong International Airport', code: 'PVG' },
-  { name: 'Munich Airport', code: 'MUC' }
 ]
 
 # Delete old records
@@ -38,7 +23,7 @@ ids = Airport.ids
   departure_airport_id = ids.sample
   arrival_airport_id = ids.sample
 
-  date = Date.today + rand(0..10).days
+  date = Date.today + rand(1..2).days
   t = Time.now + rand(0..24*60).minutes
   time = t - (t.min % 30).minutes - t.sec
 
