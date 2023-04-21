3f5c299642d9d9ed03173308f1ef18900380a619
Werner <turbotimoh@gmail.com>
2023-04-21 09:13:24 +0545

    chore: update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_044437) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_032647) do
   create_table "airports", force: :cascade do |t|
     t.string "airport_code", default: "", null: false
     t.string "airport_name", default: "", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_044437) do
     t.datetime "updated_at", null: false
     t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "bookings", "flights"
