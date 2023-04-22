002f94dea461438a8dafb87b50abd62318d14cb4
Werner <turbotimoh@gmail.com>
2023-04-22 11:02:32 +0545

    feat: add `BookingsController#new`

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,13 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+
     @flight = Flight.find(params[:flight_id])
-    @num_tickets = params[:num_tickets]
+    num_tickets = params[:num_tickets].to_i
+
+    num_tickets.times { @booking.passengers.build }
+  end
+
+  def create
   end
 end
