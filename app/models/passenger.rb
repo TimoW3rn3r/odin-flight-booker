cb26f378c2647a64ff121b74bc15e7a010fbea90
Werner <turbotimoh@gmail.com>
2023-04-23 16:23:27 +0545

    feat: add validations to `Passenger` model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,12 @@
 class Passenger < ApplicationRecord
   has_many :bookings
   has_many :flights, through: :bookings
-  
+
   belongs_to :booking
+
+  validates :name, presence: true
+  validates :email, presence: true, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    on: :create
+  }
 end
