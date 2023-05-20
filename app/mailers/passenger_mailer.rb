7b814d8862d3d119ddc0f45bff2802633b72f8c3
Werner <turbotimoh@gmail.com>
2023-05-20 12:26:49 +0545

    feat: add `#ticket_email` for `PassengerMailer`

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  default from: "noreply@flight-booker-odin.fly.dev"
+  layout 'mailer'
+
+  def ticket_email
+    @passenger = params[:passenger]
+    @flight = params[:flight]
+    mail(to: @passenger.email, subject: '[OdinFlightBooker] Flight Booked')
+  end
 end
